83922b69e366960cc6640f065bf327beb1e608e9
Dudi Zimberknopf <dudi@vizo.dev>
2023-11-07 16:16:52 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
+import { ipcMain, dialog } from "electron";
+import { createCanvas, loadImage } from "@napi-rs/canvas";
+import { scanImageData } from "@undecaf/zbar-wasm";
+import Tesseract from "node-tesseract-ocr";
+import jimp from "jimp";
+import { spawn } from "child_process";
+import * as pyshell from "python-shell";
 
 function createWindow() {
   // Create the browser window.
@@ -7,6 +14,8 @@ function createWindow() {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      contextIsolation: false,
+      nodeIntegration: true,
     },
     width: 800,
   });
@@ -42,3 +51,95 @@ app.on("window-all-closed", () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+
+ipcMain.on("scan", async (event, arg) => {
+  console.log("scan");
+  // Open file picker for images
+
+  const result = await dialog.showOpenDialog({
+    buttonLabel: "Select",
+    title: "Select an image",
+    properties: ["openFile"],
+    filters: [{ name: "Images", extensions: ["jpg", "png"] }],
+  });
+
+  if (result.canceled) {
+    console.log("No folder selected");
+    return;
+  }
+
+  extractBarcode(event, result.filePaths[0]);
+  extractText(event, result.filePaths[0]);
+});
+
+ipcMain.on("find-barcode", async (event, arg) => {
+  console.log("find-barcode");
+  // Open file picker for images
+
+  const result = await dialog.showOpenDialog({
+    buttonLabel: "Select",
+    title: "Select an image",
+    properties: ["openFile"],
+    filters: [{ name: "Images", extensions: ["jpg", "png"] }],
+  });
+
+  if (result.canceled) {
+    console.log("No folder selected");
+    return;
+  }
+
+  findBarcode(event, result.filePaths[0]);
+});
+
+async function extractBarcode(event: any, path: string) {
+  const img = await loadImage(path),
+    canvas = createCanvas(img.width, img.height),
+    ctx = canvas.getContext("2d");
+
+  ctx.drawImage(img, 0, 0);
+
+  const imageData = ctx.getImageData(0, 0, img.width, img.height) as ImageData;
+  const symbols = await scanImageData(imageData);
+
+  console.log(symbols);
+
+  console.log(symbols[0]?.typeName, symbols[0]?.decode());
+
+  event.reply("scan-reply", symbols[0].decode());
+}
+
+async function extractText(event: any, path: string) {
+  const sharpenImage = await jimp.read(path).then((image) => {
+    return image
+      .color([{ apply: "desaturate" as any, params: [90] }])
+      .contrast(1)
+      .write("img-opt.jpg");
+  });
+
+  const buffer = await sharpenImage.getBufferAsync(jimp.MIME_JPEG);
+
+  await Tesseract.recognize(buffer, {
+    lang: "eng",
+  }).then((text) => {
+    console.log(text);
+  });
+}
+
+async function findBarcode(event: any, path: string) {
+  const shell = new pyshell.PythonShell("find-barcode.py", {
+    args: [path],
+    mode: "json",
+  });
+
+  shell.on("message", (message) => {
+    console.log(message);
+    event.reply("find-barcode-reply", message);
+  });
+
+  shell.end(function (err,code,signal) {
+    if (err) throw err;
+    console.log('The exit code was: ' + code);
+    console.log('The exit signal was: ' + signal);
+    console.log('finished');
+  });
+}
--- src/renderer.ts
+++ src/renderer.ts
@@ -4,3 +4,29 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+
+import { ipcRenderer } from "electron";
+
+// const { ipcRenderer } = require('electron');
+
+const scanButton = document.querySelector("#scanButton");
+
+scanButton.addEventListener("click", () => {
+  console.log("scanButton clicked");
+
+  ipcRenderer.on("scan-reply", (event, arg) => {
+    document.querySelector("#result").innerHTML = arg;
+  });
+
+  ipcRenderer.send("scan");
+});
+
+const barcodeButton = document.querySelector("#barcodeButton");
+
+barcodeButton.addEventListener("click", () => {
+  ipcRenderer.on("find-barcode-reply", (event, arg) => {
+    document.querySelector("#result").innerHTML = arg;
+  });
+
+  ipcRenderer.send("find-barcode");
+});
